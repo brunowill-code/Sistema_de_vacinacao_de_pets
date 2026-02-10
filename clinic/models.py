fbbb58419e1917cdb8d283fdd086d1bff00e2fcd
brunowill-code <brunowilliam.code@gmail.com>
2026-02-10 10:58:07 -0300

    feat: app clinic and clinic.models created

---
+++ clinic/models.py
@@ -0,0 +1,35 @@
+from django.db import models
+
+class Clinica(models.Model):
+    nome = models.CharField(max_length=100)
+    def __str__ (self):
+        return self.nome
+
+class Tutor(models.Model):
+    clinica = models.ForeignKey(
+        Clinica,
+        on_delete=models.CASCADE,
+        related_name="tutores"
+    )
+    nome = models.CharField(max_length=100)
+    telefone = models.CharField(max_length=20)
+    email = models.CharField(max_length=50, blank=False)
+    data_de_nascimento = models.DateField()
+    cpf = models.CharField()
+
+    def __str__(self):
+        return self.nome
+    
+class Profissional(models.Model):
+    clinica = models.ForeignKey(
+        Clinica,
+        on_delete=models.CASCADE,
+        related_name="profissionais"
+    )
+    nome = models.CharField(max_length=150)
+    crmv = models.CharField(max_length=20)
+    proxima_vacina = models.CharField(max_length=150)
+    data_de_vacinacao = models.DateField()
+
+    def __str__(self):
+        return self.nome
